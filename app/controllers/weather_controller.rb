989822eec93f723b67325fd969b9e1ab8ea1cad9
SimeonGAngelov <simeonangelov56@gmail.com>
2026-01-12 19:35:15 +0200

    Dockerizing the project and minor adjustments

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,6 +1,6 @@
 class WeatherController < ApplicationController
   def index
-    @records = HourlyWeather.order(timestamp: :asc)
+    @records = HourlyWeather.order(timestamp: :desc)
     @latitude = params[:latitude]
     @longitude = params[:longitude]
   end
@@ -11,17 +11,21 @@ class WeatherController < ApplicationController
 
     current_weather = WeatherService.new.current_weather(latitude: @latitude, longitude: @longitude)
 
-    HourlyWeather.create!(
-      timestamp: Time.parse(current_weather[:time]),
+    record = HourlyWeather.find_or_initialize_by(
+      timestamp: Time.parse(current_weather[:time])
+    )
+
+    record.update!(
       latitude: current_weather[:latitude],
       longitude: current_weather[:longitude],
       temperature_2m: current_weather[:temperature],
       wind_speed_10m: current_weather[:wind_speed]
     )
 
+
     redirect_to weather_index_path
   rescue => e
-    @records = HourlyWeather.order(timestamp:).limit(200)
+    @records = HourlyWeather.order(timestamp: :desc).limit(200)
     @error_message = e.message
     render :index, status: :unprocessable_entity
   end
